01de929e6ced91e4a48cc327b13a8e1b07dd0a84
Saurabh Sakhare <saurabhs0294@gmail.com>
2024-01-02 23:21:47 +0530

    update play song on hover

--- assets/js/indexscript.js
+++ assets/js/indexscript.js
@@ -1,14 +1,19 @@
-//'use strict'
+'use strict'
 
-//window.onSpotifyIframeApiReady = (IFrameAPI) => {
-    //
-//  };
+var aud = document.getElementById('aud-player')
+var myImage = document.getElementById('my-image')
+console.log(aud)
+console.log(myImage)
+aud.currentTime = 20
 
-//  window.onSpotifyIframeApiReady = (IFrameAPI) => {
-  //  const element = document.getElementById('embed-iframe');
-   // const options = {
-     //   uri: 'spotify:episode:7makk4oTQel546B0PZlDM5'
-      //};
-    //const callback = (EmbedController) => {};
-    //IFrameAPI.createController(element, options, callback);
-  //};
+function play() {
+  aud.play();
+}
+
+function stop(){
+  aud.pause();
+}
+
+myImage.addEventListener('click', play)
+myImage.addEventListener('mouseover',play)
+myImage.addEventListener('mouseout',stop)
